a6523c11dd63fe580d1c3069dd19af8ffe82e4b4
Zach Fountain <141088304+ZachFount@users.noreply.github.com>
2023-09-16 12:09:06 -0400

    complete

--- DivByZero_dream_pulse/js/ca-scripts.js
+++ DivByZero_dream_pulse/js/ca-scripts.js
@@ -1,40 +1,52 @@
 var customerName = "Alliance  ";
 
-// <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
 function addCustomerName() {
 
-    let title = document.getElementById('title')
+    let title = document.getElementById('title');
     title.textContent = customerName;
     
-    // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
+    let h1 = document.querySelector('h1');
+    h1.textContent = customerName;
 
-    // <h2 id="introheader" class="tm-section-title mb-5 text-uppercase tm-color-primary"></h2> <!--todo: Add Customer Name-->
+    let introheader = document.getElementById('introheader');
+    introheader.textContent = customerName;
 
 };
 
 function toggleColors() {
-    // <!-- todo: Change all classes named 'tm-white-rect' to 'tm-blue-rect' -->
+
+    let whiteRects = document.querySelectorAll('.tm-white-rect');
+    whiteRects.forEach(rect => {
+        rect.classList.add('tm-blue-rect');
+        rect.classList.remove('tm-white-rect');
+    });
 
 };
 
 function hideElement() {
-    //<ul class="nav flex-row tm-social-links">  <!--todo: Hide social links-->
 
+    let socialLinks = document.getElementById('social-links');
+    socialLinks.style.visibility = 'hidden';
 };
 
  function addText() {
 
-    //<p id="intropara1" class="tm-color-gray">  <!--todo: Add customer company introduction text about their founding year-->
+    let intropara1 = document.getElementById('intropara1');
+    intropara1.textContent = 'Alliance was founded in 1931 by Albert Daniels.';
 
-    //<p id="intropara2" class="mb-0 tm-color-gray"> <!--todo: Add customer company introduction text about what they lead in-->
+    let intropara2 = document.getElementById('intropara2');
+    intropara2.textContent ='The company is a leader in P.C. Manufacturing';
 
-     //<p id="aboutpara1" id="a" class="mb-5"> <!--todo: Add customer company about text-->
+     let aboutpara1 = document.getElementById('aboutpara1');
+     aboutpara1.textContent = 'We deliver the best service this side of creation';
 
-    //<p id="aboutpara2"> <!--todo: Add customer company motto text-->
+    let aboutpara2 = document.getElementById('aboutpara2');
+    aboutpara2.textContent = 'Our motto is satisfaction guaranteed';
 
-    // <p id="addresspara" class="mb-3"> <!--todo: Add customer company phone number text-->
+    let addresspara = document.getElementById('addresspara');
+    addresspara.textContent = '1-800-565-5656';
 
-}; 
+};
 
 
 
